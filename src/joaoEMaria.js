aa84b7e8cede7d0c3cc0adf0d8fb82d08bd2ed69
gustavo costa <gustavocosta.miguel@estudante.ufjf.br>
2023-11-22 21:02:47 -0300

    termina atividade 10

---
+++ src/joaoEMaria.js
@@ -0,0 +1,38 @@
+let joao = 1;
+let maria = 2;
+
+function setJoao(valor){
+if(valor >=0){
+   joao = valor;
+}
+else{
+   joao = 0; 
+}
+}
+function setMaria(valor){
+   if(valor >=0){
+maria = valor;
+
+   }
+   maria = 0; 
+} 
+function getMaria(){
+return maria;
+
+
+}
+function getJoao(){
+return joao;
+}
+function deMariaParaJoao(){
+joao = joao + maria;
+maria = 0;
+
+}
+function deJoaoParaMaria(){
+    maria = joao + maria;
+  joao = 0;   
+    
+    }
+
+export {getJoao, getMaria, deJoaoParaMaria, deMariaParaJoao, setJoao,setMaria };
